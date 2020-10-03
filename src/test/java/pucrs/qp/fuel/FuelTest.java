de57b85f85e1ffc0a761ea351650d946f08c34f6
unknown <gustavofariasvn@hotmail.com>
2020-10-03 16:59:52 -0300

    Add new test method calculateFuel_test2

--- src/test/java/pucrs/qp/fuel/FuelTest.java
+++ src/test/java/pucrs/qp/fuel/FuelTest.java
@@ -1,12 +1,13 @@
 package pucrs.qp.fuel;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
 import org.junit.Test;
 
 public class FuelTest {
     @Test
-    public void test(){
+    public void calculateFuel_Test(){
         int s = 10;
         int a = 85;
         double expected = 70.833;
@@ -14,6 +15,15 @@ public class FuelTest {
         result = Math.floor(result * 1000) / 1000;
         assertEquals(expected, result,0);
         
+    }
+    @Test
+    public void calculateFuel_Test2(){
+        int s = 10;
+        int a = 86;
+        double expected = 70.833;
+        double result = Main.calculateFuel(s, a);
+        result = Math.floor(result * 1000) / 1000;
+        assertNotSame(expected, result);
     }
 
 }
